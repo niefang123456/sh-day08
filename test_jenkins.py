0e3a4196214f2e26ff24333f25336d9be9cc8201
niefang123456 <1184087189@qq.com>
2019-01-25 19:43:21 +0800

    allure

--- test_jenkins.py
+++ test_jenkins.py
@@ -1,9 +1,11 @@
+import allure
 class TestJenkins:
-
+	
+	@allure.step(title="test01")
     def test01(self):
         print("test01")
         assert True
-
+	@allure.step(title="test02")
     def test02(self):
         print("test02")
         assert False
